808873757f9f0ed7dac4356832f7880612428ea0
hariiish12-lab <hariiish12@gmail.com>
2025-09-08 13:29:53 +0530

    Updated some features/fixed bugs

--- server/configs/db.js
+++ server/configs/db.js
@@ -4,7 +4,7 @@ const connectDB = async () =>{
 
     try{
         mongoose.connection.on('connected',()=> console.log('Database connected'));
-        await mongoose.connect(`${process.env.MONGODB_URL}/quickshow`)
+        await mongoose.connect(`${process.env.MONGODB_URL}/quickshow3`)
 
     }catch (error){
 console.log(error.message);
